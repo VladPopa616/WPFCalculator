76c34a3cee59465098b3c91f661420a3d71a3c2c
Vlad Popa <1930022@champlaincollege.qc.ca>
2020-11-15 10:40:09 -0500

    Added OperationHandlers class to handle operations.

---
+++ WPFCalculator/OperationHandlers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFCalculator
+{
+    class OperationHandlers
+    {
+        public static double Add(double x, double y) => x + y;
+        public static double Subtract(double x, double y) => x - y;
+        public static double Multiply(double x, double y) => x * y;
+        public static double Divide(double x, double y)
+        {
+            if(y == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero");
+            }
+            return x / y; 
+        }
+    }
+}
