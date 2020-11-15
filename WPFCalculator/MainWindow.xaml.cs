a8ec8e9393fa54ee696d48bafd0259343397004b
Vlad Popa <1930022@champlaincollege.qc.ca>
2020-11-15 12:04:12 -0500

    Part 1 of event handling

--- WPFCalculator/MainWindow.xaml.cs
+++ WPFCalculator/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +22,66 @@ namespace WPFCalculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static double? lastNumber, result;
+        private static SelectedOperator? operators;
+
+        private enum SelectedOperator
+        {
+            Addition,
+            Subtraction,
+            Multiplication,
+            Division
+        }
         public MainWindow()
         {
             InitializeComponent();
+
+            foreach (var x in contents.Children)
+            {
+                if(x is Button button && !Regex.IsMatch(button.Content.ToString(), @"\d+"))
+                {
+                    button.Click += AssignClick(button.Content.ToString());
+                }
+            }
+        }
+
+        private RoutedEventHandler AssignClick(string content)
+        {
+            const string operation = @"^(\+|\-|\*|\/)$";
+            const string specialOperation = @"^(\+\/\-|\%|AC|=|\.)$";
+
+            RoutedEventHandler events = (a, b) => Trace.WriteLine("Unknown");
+
+            if (Regex.IsMatch(content, operation))
+            {
+                events = (a, b) => HandleOperation(content);
+            }
+            else if (Regex.IsMatch(content, specialOperation))
+            {
+                events = AssignSpecialOperationHandler(content);
+            }
+            return events;
+        }
+
+        private void HandleSecondNumber()
+        {
+            if (result == null) return;
+            result = null;
+            Result.Content = "0";
+        }
+
+        private void HandleOperation(string operation)
+        {
+            result = null;
+
+            switch (operation)
+            {
+                case "+": operators = SelectedOperator.Addition; break;
+                case "-": operators = SelectedOperator.Subtraction; break;
+                case "*": operators = SelectedOperator.Multiplication; break;
+                case "/": operators = SelectedOperator.Division; break;
+
+            }
         }
     }
 }
